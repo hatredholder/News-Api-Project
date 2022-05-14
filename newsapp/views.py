4da1c0c0ce0ffe46d1de5df4d7f98ea7d31855cf
hatredholder <the.great.justice@bk.ru>
2022-05-14 21:55:41 +0300

    added index.html

--- newsapp/views.py
+++ newsapp/views.py
@@ -5,4 +5,15 @@ from django.shortcuts import render
 def index(request):
     r = requests.get("https://api.mediastack.com/v1/news?access_key=YOUR_ACCESS_KEY&countries=ru")
     res = r.json()
-    data =
+    data = res['data']
+    title = []
+    description = []
+    image = []
+    url = []
+    for i in data:
+        title.append(i['title'])
+        description.append(i['description'])
+        image.append(i["image"])
+        url.append(i["url"])
+    news = zip(title, description, image, url)
+    return render(request, 'newsapp/index.html', {'news':news})
